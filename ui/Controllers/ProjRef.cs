63c3a2d92482238a2cd4d20c33766665d17bcbed
Konstantin Polyzois <konstantin.polyzois@nasdaqomx.com>
2012-11-07 08:18:43 +0100

    Starting actual update

---
+++ ui/Controllers/ProjRef.cs
@@ -0,0 +1,9 @@
+﻿namespace ui.Controllers
+{
+    public class ProjRef
+    {
+        public string Name { get; set; }
+        public string Ref { get; set; }
+        public string OldRef { get; set; }
+    }
+}
---
+++ ui/Controllers/ProjRefs.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ui.Controllers
+{
+    public class ProjRefs
+    {
+        private XNamespace _ns = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+
+        public string initialDir { get; set; }
+
+        public ProjRef[] GetAllRefs()
+        {
+
+            var projFiles = ProjFiles();
+
+            XDocument doc = null;
+
+            List<ProjRef> allRefs= new List<ProjRef>();
+
+            foreach (var projFile in projFiles)
+            {
+                Console.WriteLine(projFile);
+                doc = XDocument.Load(projFile);
+
+                IEnumerable<XElement> result = (from refs in doc.Root.Descendants(_ns + "Reference")
+                                                let element = refs.Element(_ns + "HintPath")
+                                                where element != null //&& testExpr(element.Value)
+                                                select element);
+
+               
+
+                foreach (XElement hintPath in result)
+                {
+                    allRefs.Add(new ProjRef{Name = projFile,Ref = hintPath.Value});
+                }
+
+            }
+
+            return allRefs.ToArray();
+        }
+
+        private string[] ProjFiles()
+        {
+            var projFiles = Directory.GetFiles(initialDir,
+                                               "*.csproj",
+                                               SearchOption.AllDirectories);
+            List<string> filtered = new List<string>();
+            foreach (var projFile in projFiles)
+            {
+                if (!projFile.StartsWith(initialDir + "\\.git"))
+                {
+                    filtered.Add(projFile);
+                }
+            }
+            return filtered.ToArray();
+        }
+
+        private bool testExpr(string value)
+        {
+            return value.ToUpperInvariant().Contains(RefSearchPattern.ToUpperInvariant());
+        }
+
+        public string RefSearchPattern { get; set; }
+    }
+}
